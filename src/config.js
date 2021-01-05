5769cadb1e92ba1d225ca357cd548a02254cd5a3
Travis French <travis.french@alchemysystems.com>
2021-01-05 00:45:00 -0600

    Adding redirects file

--- src/config.js
+++ src/config.js
@@ -1,20 +1,44 @@
-const config = {
+const dev = {
   STRIPE_KEY: "pk_test_E7OYatBDLlr5Q8mE95tAO35K",
-  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-east-1",
-    BUCKET: "notes-app-upload-travster217",
+    BUCKET: "dev-notes-infra-s3-uploads4f6eb0fd-blja9u141wlz"
   },
   apiGateway: {
-    REGION: "us-east-2",
-    URL: "https://feh75699s2.execute-api.us-east-2.amazonaws.com/prod",
+    REGION: "us-east-1",
+    URL: "https://c8ia8hef63.execute-api.us-east-1.amazonaws.com/dev"
   },
   cognito: {
-    REGION: "us-east-2",
-    USER_POOL_ID: "us-east-2_WwETA5TK0",
-    APP_CLIENT_ID: "5n1naimh4cbv461jusbm36t49o",
-    IDENTITY_POOL_ID: "us-east-2:b5c66fb9-9dd9-4828-9ac7-97ff962752d2",
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_pfTPjMft0",
+    APP_CLIENT_ID: "7q10k4b6ntvq3a67fjdn1k3ibl",
+    IDENTITY_POOL_ID: "us-east-1:c7fec469-4fbb-46e6-baf3-f513888ccfb7"
+  }
+};
+
+const prod = {
+  STRIPE_KEY: "pk_test_E7OYatBDLlr5Q8mE95tAO35K",
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "prod-notes-infra-s3-uploads4f6eb0fd-1q45nvfzudbxb"
   },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://px0lpjtf5b.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: " us-east-1_k4KET61JE",
+    APP_CLIENT_ID: "31p3e669lfc3ij8q463em60rg4",
+    IDENTITY_POOL_ID: "us-east-1:c952c6ee-5b06-4a8b-b5a2-1de30d803a1c"
+  }
+};
+
+const config = {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  // Default to dev if not set
+  ...(process.env.REACT_APP_STAGE === "prod" ? prod : dev),
 };
 
 export default config;
